05c305d584631d574a6c0bf3f40a20334fc1eef0
alexromanov <al8x.romanov@gmail.com>
2019-10-23 20:57:23 +0300

    Add service modules. Fix mvn build.

---
+++ bonus-conference-service/src/main/java/com/alexromanov/bonusconferenceservice/BonusConferenceServiceApplication.java
@@ -0,0 +1,11 @@
+package com.alexromanov.bonusconferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusConferenceServiceApplication {
+	public static void main(String[] args){
+		SpringApplication.run(BonusConferenceServiceApplication.class, args);
+	}
+}
---
+++ user-conference-service/src/main/java/com/alexromanov/userconferenceservice/UserConferenceServiceApplication.java
@@ -0,0 +1,11 @@
+package com.alexromanov.userconferenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserConferenceServiceApplication {
+	public static void main(String[] args){
+		SpringApplication.run(UserConferenceServiceApplication.class, args);
+	}
+}
