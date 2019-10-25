c6183bfb2723f10dc25abbed0efd5946b01cda02
alexromanov <al8x.romanov@gmail.com>
2019-10-25 09:31:42 +0300

    Updated tests for user-service

--- user-conference-service/src/test/java/com/alexromanov/userconferenceservice/UserConferenceServiceComponentTest.java
+++ user-conference-service/src/test/java/com/alexromanov/userconferenceservice/UserConferenceServiceComponentTest.java
@@ -3,7 +3,6 @@ package com.alexromanov.userconferenceservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,12 +23,20 @@ public class UserConferenceServiceComponentTest {
     @Test
     public void shouldReturnUserInformationIfUserIsFound() throws Exception {
         mockMvc.perform(get("/user/111111"))
-               .andExpect(status().isOk()).andDo(print());
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id").value(111111))
+               .andExpect(jsonPath("$.name").value("Jack"))
+               .andExpect(jsonPath("$.surname").value("Ice"))
+               .andExpect(jsonPath("$.bonuses").isEmpty());
     }
 
     @Test
     public void shouldReturnEmptyInformationIfUserNotFound() throws Exception {
         mockMvc.perform(get("/user/2"))
-               .andExpect(status().isOk()).andDo(print());
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id").value(0))
+               .andExpect(jsonPath("$.name").isEmpty())
+               .andExpect(jsonPath("$.surname").isEmpty())
+               .andExpect(jsonPath("$.bonuses").isEmpty());
     }
 }
